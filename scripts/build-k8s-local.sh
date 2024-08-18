f70634081a6d303d04d152733acc8a99d9ebf05a
Hung Nguyen <nxhung16102001@gmail.com>
2024-08-18 08:53:29 +0700

    ref: scripts

--- scripts/build-k8s-local.sh
+++ scripts/build-k8s-local.sh
@@ -2,6 +2,8 @@ cd ../k8s/ || exit
 
 export $(grep -v '^#' .env | xargs)
 
+envsubst < namespace.yaml | kubectl apply -f -
+
 for file in *-datasource.yaml; do
   envsubst < "$file" | kubectl apply -f -
 done
--- scripts/build-k8s.sh
+++ scripts/build-k8s.sh
@@ -1,5 +1,7 @@
 cd ../k8s/ || exit
 
+envsubst < namespace.yaml | kubectl apply -f -
+
 for file in *-datasource.yaml; do
   envsubst < "$file" | kubectl apply -f -
 done
